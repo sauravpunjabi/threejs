0b07c87a39a47fc763c447dc22a2e09d1da7d485
Saurav <sauravpunjabi123@gmail.com>
2026-06-13 11:52:00 +0530

    animation

--- r3f-pract/src/App.jsx
+++ r3f-pract/src/App.jsx
@@ -1,33 +1,42 @@
-import {Canvas} from '@react-three/fiber'
+import {useRef} from 'react'
+import {Canvas, useFrame} from '@react-three/fiber'
 import './App.css'
+
+const Cube = ({ position, size, color }) => {
+const ref = useRef()
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta
+    ref.current.rotation.y += delta * 3
+    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2 
+  })
+
+  return(
+      <mesh position={position} ref={ref}>
+        <boxGeometry args={size} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+
+  )
+}
 
 function App() {
   return(
     <Canvas>
 
 
-      <directionalLight position={[0,0, 2]}/>
+      <directionalLight position={[0,0, 2]} intensity={0.5}/>
+      <ambientLight />
 
-      <mesh position={[1, 0, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color={"red"} />
-      </mesh>
+     {/*  <group position={[0, -1, 0]}>
+        <Cube position={[1,0,0]} color={"orange"} />
+        <Cube position={[-1,0,0]} color={"pink"} />
+        <Cube position={[-1,2,0]} color={"red"} />
+        <Cube position={[1,2,0]} color={"purple"} />
+      </group> */}
 
-       <mesh position={[-1, 0, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color={"red"} />
-      </mesh>
 
-       <mesh position={[-1, 2, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color={"red"} />
-      </mesh>
-
-       <mesh position={[1, 2, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color={"red"} />
-      </mesh>
-      
+      <Cube position={[0,0,0]} color={"red"} />
     </Canvas>
   )
 }
