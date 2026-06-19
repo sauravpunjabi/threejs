725608d0e594146cfad5fa4d19527d389b30ec7a
Saurav <sauravpunjabi123@gmail.com>
2026-06-19 20:10:47 +0530

    added smooth scroller

--- animations/src/App.jsx
+++ animations/src/App.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './sections/Hero'
-import { ScrollTrigger } from 'gsap/all';
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import gsap from 'gsap';
 import Message from './sections/Message';
 import Flavors from './sections/Flavors';
+import { useGSAP } from '@gsap/react';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
+
   return (
     <main>
         <NavBar />
-        <Hero />
-        <Message />
-        <Flavors />
-        <div className="border border-red-500 h-screen"></div>
+
+        <div id="smooth-wrapper">
+          <div id="smooth-content">
+            <Hero />
+            <Message />
+            <Flavors />
+            <div className="border border-red-500 h-screen" />
+          </div>
+        </div>
+        
     </main>
   )
 }
